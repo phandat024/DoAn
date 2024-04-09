1447309e91014a57be13f10b3e92e59ee244f051
ThienBao1610 <bcao1210@gmail.com>
2024-04-09 17:41:38 +0700

    database

--- DoAn/database/migrations/2014_10_12_000000_create_users_table.php
+++ DoAn/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            //$table->id();
+           // $table->string('avatar')->nullable();
+           // $table->string('username')->unique();
+           // $table->string('name');
+            $table->string('email', 100)->unique();
+           // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            //$table->rememberToken();
+           // $table->timestamp('login_at')->nullable();
+           // $table->timestamp('change_password_at')->nullable();
             $table->timestamps();
         });
     }
