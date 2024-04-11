d2c0ac16361d8cb9920d98eb708b366f21972088
Quoc-Khiem <khiembuiquoc@gmail.com>
2024-04-11 15:32:42 +0700

    da

---
+++ DoAn/resources/views/crud/delete.blade.php
@@ -0,0 +1,41 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <h1>Quản lý người dùng</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->password }}</td>
+                                <td>
+                                    <form action="{{ route('user.deleteUser', ['id' => $user->id]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit">Xóa</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+@endsection
